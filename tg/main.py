153978813969f18eb1d6565aa8af7c6d43bcc063
kuderr <dakudryavcev@gmail.com>
2021-05-15 23:01:27 +0300

    files message

--- tg/main.py
+++ tg/main.py
@@ -1,9 +1,8 @@
-import json
-
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.dispatcher.filters import Text
 from aiogram.utils import executor
+from hurry.filesize import size
 
 from tg.core import psql
 from tg.core.settings import settings
@@ -12,6 +11,17 @@ TOKEN = settings.token
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
+
+mysystem = [
+    (1024 ** 5, " ПБ"),
+    (1024 ** 4, " ТБ"),
+    (1024 ** 3, " ГБ"),
+    (1024 ** 2, " МБ"),
+    (1024 ** 1, " КБ"),
+    (1024 ** 0, " Б"),
+]
+
+statuses = {"created": "грузится", "loading": "грузится", "done": "загружен"}
 
 
 @dp.message_handler(commands=["start"])
@@ -41,8 +51,13 @@ async def sum_files(message: types.Message):
     count = raw_count["count"]
 
     if user_id:
-        await bot.send_message(message.from_user.id, f"У тебя {count} загруженных файлов")
-        await bot.send_message(message.from_user.id, f"Вес всех загруженных файлов - {sum} байт")
+        await bot.send_message(
+            message.from_user.id, f"У тебя {count} загруженных файлов"
+        )
+        await bot.send_message(
+            message.from_user.id,
+            f"Вес всех загруженных файлов - {size(sum, system=mysystem)}",
+        )
     else:
         await bot.send_message(
             message.from_user.id,
@@ -55,33 +70,31 @@ async def all_files(message: types.Message):
     raw_id = await psql.get_user_id(message.from_user.username)
     files = await psql.get_files(raw_id["id"])
 
-    dict_files = [dict(
-        id=file["id"],
-        name=file["name"],
-        size=file["size"],
-        status=file["status"]
-    ) for file in files]
-    json_files = json.dumps(dict_files, indent=2)
+    template = "{name}\nРазмер: {file_size}\nСтатус: {status}\n"
 
-    md_string = f"```json\n{json_files}\n```"
+    md_string = "\n".join(
+        [
+            template.format(
+                name=file["name"],
+                file_size=size(file["size"], system=mysystem),
+                status=statuses[file["status"]],
+            )
+            for file in files
+        ]
+    )
 
     if id:
         if len(files) == 0:
             await bot.send_message(
                 message.from_user.id,
-                f"{message.from_user.username}, у тебя нет загруженных файлов"
+                f"{message.from_user.username}, у тебя нет загруженных файлов",
             )
 
         await bot.send_message(
-            message.from_user.id,
-            f"{message.from_user.username}, твои файлы:"
+            message.from_user.id, f"{message.from_user.username}, твои файлы:"
         )
 
-        await bot.send_message(
-            message.from_user.id,
-            md_string,
-            parse_mode="Markdown"
-        )
+        await bot.send_message(message.from_user.id, md_string)
     else:
         await bot.send_message(
             message.from_user.id,
