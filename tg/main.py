f858f6d00925768ced0aef00801ce9ca3cabb4f7
Burnouttt <burnouttt@yandex.ru>
2021-05-15 20:52:16 +0300

    all_files update

--- tg/main.py
+++ tg/main.py
@@ -1,12 +1,12 @@
+import json
+
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
-from aiogram.utils import executor
-from loguru import logger
 from aiogram.dispatcher.filters import Text
+from aiogram.utils import executor
 
-from tg.core.settings import settings
 from tg.core import psql
-
+from tg.core.settings import settings
 
 TOKEN = settings.token
 
@@ -32,11 +32,17 @@ async def process_help_command(message: types.Message):
 @dp.message_handler(Text(equals="Общий обьем файлов"))
 async def sum_files(message: types.Message):
     raw_id = await psql.get_user_id(message.from_user.username)
-    id = raw_id["id"]
-    raw_sum = await psql.get_files_size(id)
+    user_id = raw_id["id"]
+
+    raw_sum = await psql.get_files_size(user_id)
     sum = raw_sum["sum"]
-    if id:
-        await bot.send_message(message.from_user.id, f"Вес всех файлов - {sum}")
+
+    raw_count = await psql.get_files_count(user_id)
+    count = raw_count["count"]
+
+    if user_id:
+        await bot.send_message(message.from_user.id, f"У тебя {count} загруженных файлов")
+        await bot.send_message(message.from_user.id, f"Вес всех загруженных файлов - {sum} байт")
     else:
         await bot.send_message(
             message.from_user.id,
@@ -46,10 +52,35 @@ async def sum_files(message: types.Message):
 
 @dp.message_handler(Text(equals="Показать все файлы"))
 async def all_files(message: types.Message):
-    id = await psql.get_user_id(message.from_user.username)
+    raw_id = await psql.get_user_id(message.from_user.username)
+    files = await psql.get_files(raw_id["id"])
+
+    dict_files = [dict(
+        id=file["id"],
+        name=file["name"],
+        size=file["size"],
+        status=file["status"]
+    ) for file in files]
+    json_files = json.dumps(dict_files, indent=2)
+
+    md_string = f"```json\n{json_files}\n```"
+
     if id:
+        if len(files) == 0:
+            await bot.send_message(
+                message.from_user.id,
+                f"{message.from_user.username}, у тебя нет загруженных файлов"
+            )
+
         await bot.send_message(
-            message.from_user.id, f"{message.from_user.username} - {id}, салам!"
+            message.from_user.id,
+            f"{message.from_user.username}, твои файлы:"
+        )
+
+        await bot.send_message(
+            message.from_user.id,
+            md_string,
+            parse_mode="Markdown"
         )
     else:
         await bot.send_message(
